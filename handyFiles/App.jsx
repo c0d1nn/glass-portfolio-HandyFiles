bb20e9f816267623f5dfab0a499942819d876782
Codin Zero <153553122+c0d1nn@users.noreply.github.com>
2024-03-28 15:55:45 +0100

    Add files via upload

---
+++ handyFiles/App.jsx
@@ -0,0 +1,22 @@
+import About from "./components/About"
+import Contact from "./components/Contact"
+import Footer from "./components/Footer"
+import Hero from "./components/Hero"
+import Navbar from "./components/Navbar"
+import Work from "./components/Work"
+
+function App() {
+
+  return (
+    <>
+    <Navbar/>
+    <Hero/>
+    <Work/>
+    <About/>
+    <Contact/>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
